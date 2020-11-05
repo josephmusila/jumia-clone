38e9b83f6c7a6515e465b759ac8e6ba51126a6ef
joseph musila <josemusila03@gmail.com>
2020-11-05 16:55:27 +0300

    commit

--- index.js
+++ index.js
@@ -9,11 +9,23 @@ $(document).ready(function(){
     $("#cart").click(function(){
         $("#cart-det").slideToggle();
     });
-    var elec=$("#electronics");
+  /*   var elec=$("#electronics");
     $("#nav-ul li:nth-child(1)").click(function(){
         $("#items-placeholder").append(elec);
     });
     $("#nav-ul li:nth-child(2)").click(function(){
         $("#clothes").show();
-    })
+    }) */
+    const elec=["tvs","cameras", "cables","cameras","wifi","tv holders","batteries"];
+    const clothes=["men wear","women wear", "kids", "make up", "sports", "miscallaneous"];
+    const books=["exercise books", "primary school", "religion", "advanced"];
+    const furniture=["indoor","outdoor","gym","office"];
+
+  $("#nav-ul li:nth-child(1)").click(function(){
+       $.each(elec,function(index,value){
+           $("#items-placeholder").append( "<div>" + index + ":" + value +  "</div>");
+          
+       })
+    }) 
+
 });
